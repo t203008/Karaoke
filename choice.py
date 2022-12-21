cf93449a0a3d6f9d37deef939b49bc857b93698d
t203008 <91816208+t203008@users.noreply.github.com>
2022-12-21 12:21:41 +0900

    Update choice.py

--- choice.py
+++ choice.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
-theme1=pd.read_csv("Meet up without Smartphone.csv")
-x=st.number_input("選んだ数字", 0, 10000, 0)
-theme1=theme1.sort_values("Random")
-x=x % len(theme1)
 st.title("スマホなしで待ち合わせ　お題")
-st.write(theme1.iloc[x][0])
+theme1=pd.read_csv("Meet up without Smartphone.csv")
+x=st.number_input("選んだ数字", 0, 10000, -10000)
+theme1=theme1.sort_values("Random")
+if x!=0:
+  x=x % len(theme1)
+  st.write(theme1.iloc[x][0])
