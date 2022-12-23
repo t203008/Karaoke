9c8a7be67ed2de5cb6e18120e7c7dfc1758ae2cf
t203008 <91816208+t203008@users.noreply.github.com>
2022-12-23 23:54:29 +0900

    Update choice.py

--- choice.py
+++ choice.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 st.title("スマホなしで待ち合わせ　お題")
-st.write("365870_0.jpg")
+st.write(365870_0.jpg)
 theme1=pd.read_csv("Meet up without Smartphone.csv")
 x=st.number_input("選んだ数字", -10000, 10000, 0)
 theme1=theme1.sort_values("Random")
